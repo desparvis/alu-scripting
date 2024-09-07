74b867a10547cb352c0615cbbc3e2d798c27793e
desparvis <latifahm501@gmail.com>
2024-09-07 14:22:23 -0700

    r

---
+++ regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Checking if one argument is passed
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Getting the input string
+input = ARGV[0]
+
+# Useing a regular expression to find all the matches  of 'School'
+matches = input.scan(/h.n/)
+
+# Printing the matches combined
+puts matches.join
