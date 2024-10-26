d2e4c36c4441924360effc65b4288724c3e0b16c
desparvis <latifahm501@gmail.com>
2024-10-26 19:54:37 +0000

    r

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,17 +2,16 @@
 """DOCUMENTING"""
 import requests
 
-
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()['data']
         for post in data['children'][:10]:
             print(post['data']['title'])
+        return "OK"
     else:
-        print('OK')
+        return "OK"
